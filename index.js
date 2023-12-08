70a110e8b68978cfc8e08bd6190106e6db91ffd2
plticmd <86671100+plticmd@users.noreply.github.com>
2023-12-08 15:40:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 import { Client, dropsToXrp, rippleTimeToISOTime } from 'xrpl';
 
-import addXrplLogo from './src/helpers/render-xrpl-logo';
 import getWalletDetails from './src/helpers/get-wallet-details.js';
-
-// Optional: Render the XRPL logo
-addXrplLogo();
 
 const client = new Client(process.env.CLIENT); // Get the client from the environment variables
 
 // Get the elements from the DOM
-const sendXrpButton = document.querySelector('#send_xrp_button');
-const txHistoryButton = document.querySelector('#transaction_history_button');
-const escrowButton = document.querySelector('#escrow_button');
 const walletElement = document.querySelector('#wallet');
 const walletLoadingDiv = document.querySelector('#loading_wallet_details');
 //const ledgerLoadingDiv = document.querySelector('#loading_ledger_details');
-
-// Add event listeners to the buttons
-sendXrpButton.addEventListener('click', () => {
-    window.location.pathname = '/src/send-xrp/send-xrp.html';
-});
-
-txHistoryButton.addEventListener('click', () => {
-    window.location.pathname = '/src/transaction-history/transaction-history.html';
-});
-
-escrowButton.addEventListener('click', () => {
-    window.location.pathname = '/src/escrow/escrow.html';
-});
 
 // Self-invoking function to connect to the client
 (async () => {
@@ -56,18 +36,6 @@ escrowButton.addEventListener('click', () => {
             .finally(() => {
                 walletLoadingDiv.style.display = 'none';
             });
-
-
-        // Fetch the latest ledger details
-        // client.on('ledgerClosed', (ledger) => {
-        //     ledgerLoadingDiv.style.display = 'none';
-        //     const ledgerIndex = document.querySelector('#ledger_index');
-        //     const ledgerHash = document.querySelector('#ledger_hash');
-        //     const closeTime = document.querySelector('#close_time');
-        //     ledgerIndex.textContent = `Ledger Index: ${ledger.ledger_index}`;
-        //     ledgerHash.textContent = `Ledger Hash: ${ledger.ledger_hash}`;
-        //     closeTime.textContent = `Close Time: ${rippleTimeToISOTime(ledger.ledger_time)}`;
-        // });
         
     } catch (error) {
         await client.disconnect();
